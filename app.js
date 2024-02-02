51fcaebc3f3a5b797027235ace8cd2a2988bd1a9
Mei <e2296236@cmaisonneuve.qc.ca>
2024-02-02 14:29:38 -0500

    Terminer cours-4

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 const path = require("path");
 const mustacheExpress = require("mustache-express");
 const db = require("./config/db.js");
+const { ServerResponse } = require("http");
 
 // au début du fichier - configuration
 dotenv.config();
@@ -47,31 +48,40 @@ server.engine("mustache", mustacheExpress());
 // get tous les fichiers statiques dans le dossier public (donne accès à tout ce qui est dans le dossier public pour protéger le reste du projet)
 server.use(express.static(path.join(__dirname, "public")));
 
+//Permet d'accepter des bodys en Json dans les requêtes
+server.use(express.json());
+
 // Points d'accès
 server.get("/donnees", async (req, res)=>
 {
-    // const test = {email: "test@gmail.com"}
-    console.log(req.query);
+    try
+    {    // const test = {email: "test@gmail.com"}
+        console.log(req.query);
 
-    // Ceci sera remplacé par un fetch ou un appel à la base de données
-    // const donnees = require("./data/donneesTest");
-    const direction = req.query["order-direction"] || 'asc';
-    // Ne peut pas enchaîner une clé avec tiret, pas exemple : "req.query.order-direction"
-    const limit = +req.query.limit || 1000 ;
-    // La signe plus convertir à un nombre 
+        // Ceci sera remplacé par un fetch ou un appel à la base de données
+        // const donnees = require("./data/donneesTest");
+        const direction = req.query["order-direction"] || 'asc';
+        // Ne peut pas enchaîner une clé avec tiret, pas exemple : "req.query.order-direction"
+        const limit = +req.query.limit || 1000 ;
+        // La signe plus convertir à un nombre 
 
-    const donneesRef = await db.collection("test").orderBy("user", direction).limit(limit).get();
+        const donneesRef = await db.collection("test").orderBy("user", direction).limit(limit).get();
 
-    const donneesFinale = [];
+        const donneesFinale = [];
 
-    donneesRef.forEach((doc)=>
+        donneesRef.forEach((doc)=>
+        {
+            donneesFinale.push(doc.data());
+        });
+
+        res.statusCode = 200;
+        res.json(donneesFinale);
+    }
+    catch(e)
     {
-        donneesFinale.push(doc.data());
-    });
-
-    res.statusCode = 200;
-    res.json(donneesFinale);
-
+        res.statusCode = 500;
+        res.json({message : 'Une erreur est survenue.'});
+    }
 })
 
 /**
@@ -80,29 +90,136 @@ server.get("/donnees", async (req, res)=>
  * Permet d'accéder à un utilisateur
  */
 // : implique que c'est un paramètre, ce qui suive le : est la cle de l'array 'params'
-server.get("/donnees/:id", (req, res)=>
+server.get("/donnees/:id", async(req, res)=>
 {
-    // console.log(req.params.id);
-    const donnees = require("./data/donneesTest");
-    // find retourne l'element utilisateur
-    const utilisateur = donnees.find((element)=>
+    try 
+    {    // console.log(req.params.id);
+        const id = req.params.id;
+        // const donnees = require("./data/donneesTest");
+        const doc = await db.collection('test').doc(id).get();
+        // find retourne l'element utilisateur
+        const utilisateur = doc.data();
+
+        if(utilisateur)
+        {
+            res.statusCode = 200;
+            res.json(utilisateur);
+        }
+        else
+        {
+            res.statusCode = 404;
+            res.json({ message: "Utilisateur non trouvé. "});
+        }
+    }
+    catch(e)
     {
-        return element.id == req.params.id;
+        res.statusCode = 500;
+        res.json({message : 'Une erreur est survenue.'})
+    }
+});
+
+/**
+ * @method POST
+ */
+
+server.post('/donnees', async (req, res)=>
+{
+    try
+    {    
+        const test = req.body;
+
+        // Validation des données
+        if(test.user == undefined)
+        {
+            res.statusCode = 400;
+            return res.json({message: 'Vous devez fournir un utilisateur.'});
+        }
+
+        await db.collection('test').add(test);
+
+        res.statusCode = 201;
+        res.json({message: 'la donnée a été ajoutée', donnees: test});
+    }
+    catch(e)
+    {
+        res.statusCode = 500;
+        res.json({message : 'Une erreur est survenue.'});
+    }
+})
+
+/**
+ * @method POST
+ * Initialiser données depuis un fichier vers la base de données
+ */
+server.post("/donnees/initialiser", (req, res)=>
+{
+    const donnesTest = require("./data/donneesTest.js");
+
+    donnesTest.forEach(async(element)=>
+    {
+        await db.collection('test').add(element);
     })
 
-    if(utilisateur)
-    {
+    res.statusCode = 200;
+    res.json({message: "Données initialisées"})
+})
+
+/**
+ * @method PUT
+ */
+server.put('donnees/:id', async (req, res)=>
+{
+   try{ 
+        const id = req.params.id;
+
+        const donneeModifiees = req.body;
+
+        //Validation ici
+
+        await db.connection('test').doc(id).update(donneeModifiees);
+
         res.statusCode = 200;
-        res.json(utilisateur);
+        res.json({message: "La donnée a été modifée."});
     }
-    else
+    catch(e)
     {
-        res.statusCode = 404;
-        res.json({ message: "Utilisateur non trouvé. "});
+        res.statusCode = 500;
+        res.json({message : 'Une erreur est survenue.'});
     }
-    console.log(req);
-    res.send(req.params.id);
-});
+})
+
+/**
+ * @method Delete
+ */
+server.delete('/donnees/:id', async (req, res)=>
+{
+    try{ 
+        const id = req.params.id;
+        // const donnees = require("./data/donneesTest");
+        const doc = await db.collection('test').doc(id).get();
+        // find retourne l'element utilisateur
+        const utilisateur = doc.data();
+
+        if(utilisateur)
+        {
+            const resultat = await db.collection('test').doc(id).delete();
+    
+            res.statusCode = 200;
+            res.json({message: 'Le document a été supprimé.'})
+        }
+        else
+        {
+            res.statusCode = 404;
+            res.json({ message: "Utilisateur non trouvé. "});
+        }
+    }
+    catch(e)
+    {
+        res.statusCode = 500;
+        res.json({message : 'Une erreur est survenue.'});
+    }
+})
+
 
 // Doit être le dernier
 // Gestion page 404 - requête non trouvée
